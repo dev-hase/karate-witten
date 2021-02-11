43838b7a9c6b98fcf058220485504967b29a218d
Daniel Schmidt <2wusel@web.de>
2021-02-11 10:50:20 +0100

    404

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="theme-container">
+    <div class="theme-default-content">
+      <h1>Seite nicht gefunden!</h1>
+      <img src="/img/404.jpg" alt="404" />
+      <blockquote>{{ getMsg() }}</blockquote>
+      
+      <RouterLink to="/">
+        zurück zur Startseite
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script>
+const msgs = [
+  `Wie bin ich hierhin gekommen?!?`,
+  `Diesen Link gibt's leider nicht...`
+]
+
+export default {
+  methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    }
+  }
+}
+</script>
